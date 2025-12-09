a0efd727f2b40ff96f5dbeaf673235f93f362e71
ayoub <ab.naciri.official@gmail.com>
2025-12-09 22:39:33 +0100

    feat: add filter inputString function

--- src/brain-fuck.c
+++ src/brain-fuck.c
@@ -1,17 +1,45 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
+// *filtering brain code 
 
-
-
-int main()
+char *filtreString(char *inputString)
 {
+    int stringLenght = strlen(inputString);
+    char *newString = calloc(stringLenght,1);
+    int i = 0;
+    int j = 0;
+    
+
+    while(i < stringLenght)
+    {
+        if(inputString[i] == '+' || inputString[i] == '-' || inputString[i] == '>' 
+            || inputString[i] == '<' || inputString[i] == '[' || inputString[i] == ']' 
+            ||inputString[i] == '.')
+        {
+            newString[j] = inputString[i];
+            j++;
+        }
+        i++;
+    }
+    return newString;
+}
+
+
+int main(int argc,char **argv)
+{
+    //check the nember of arguments
+    if(argc != 2 )
+    {
+        printf("invalid number of arguments\n");
+        return 0;
+    }
 
     void *programe;
     programe = calloc(30000,1);
-
-    printf("hello in c");
-
+    char *new = filtreString(argv[1]);
+    free(new);
     free(programe);
     return (0);
 }
