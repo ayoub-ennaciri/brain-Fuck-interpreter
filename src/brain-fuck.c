0329290b85d53b2697472cb2847401d8d9baae3f
ayoub <ab.naciri.official@gmail.com>
2025-12-10 23:36:57 +0100

    feat: add getting number of squar brackets and storing them an there position function

--- src/brain-fuck.c
+++ src/brain-fuck.c
@@ -52,11 +52,83 @@ void run(void *prBuffer,char *prCode)
         }
         if(prCode[i] == '.')
         {
+            printf("%p : %d\n",(char *)prBuffer,*(char *)prBuffer);
             write(1,(char *)prBuffer,1);
         }
         i++;
     }
     printf("\nprnt %s\n",(char *)(prBuffer - 2) );
+}
+
+// *get the number of square brackets
+char ***squareBrackets(char *prCode)
+{
+    int lenght = strlen(prCode);
+    int numberOfLeft = 0;
+    int numberOfRight = 0;
+    int i = 0;
+    // ? getting number of left and right square brackets
+    printf("lenght : %ld\n",strlen(prCode));
+    while(i < lenght)
+    {
+        if(prCode[i] == '['){
+            numberOfLeft++;
+        }
+        if(prCode[i] == ']'){
+            numberOfRight++;
+        }
+        i++;
+    }
+    printf("left br: %d\nright br: %d\n",numberOfLeft,numberOfRight);
+    // ? checking if they are aqual
+    if(numberOfLeft != numberOfRight || !numberOfLeft || !numberOfRight ){
+        printf("Syntax Error : FIN MA3ART\n");
+        return NULL;
+    }
+    
+    // ? allocating for left brackets
+    char **bracketLeft = calloc(numberOfLeft, sizeof(char *));
+    if(!bracketLeft)
+    {
+        return NULL;
+    }
+
+    // ? allocating for right brackets
+    char **bracketRight = calloc(numberOfLeft, sizeof(char *));
+    if(!bracketRight)
+    {
+        free(bracketLeft);
+        return NULL;
+    }
+
+    int j = 0;
+    int h = 0;
+    i = 0;
+
+    while(i < lenght)
+    {
+        if(prCode[i] == '['){
+            bracketLeft[j] = &prCode[i];
+            printf("left bracket at position %d\nstrored at adrr: %p\n",i,bracketLeft[j]);
+            j++;
+        }
+        i++;
+    }
+
+    while(i >= 0)
+    {
+        if(prCode[i] == ']'){
+            bracketRight[h] = &prCode[i];
+            printf("right bracket at position %d\nstrored at adrr: %p\n",i,bracketRight[h]);
+            h++;
+        }
+        i--;
+    }
+
+    char ***bracketArray = calloc(2,sizeof(char ***));
+    bracketArray[0] = bracketLeft;
+    bracketArray[1] = bracketRight;
+    return bracketArray;
 }
 
 int main(int argc,char **argv)
@@ -68,13 +140,26 @@ int main(int argc,char **argv)
         return 0;
     }
 
-
-    printf("aus dial 4 : %d\n",pow(4,2));
-
+    // alloc program buffer
     void *program;
     program = calloc(30000,1);
+    if(!program)
+    {
+        return 0;
+    }
+
+    // filtering the input string from unwanted chars
     char *new = filtreString(argv[1]);
+
+    // getting the addresses of the squar brackets
+    char ***array = squareBrackets(new);
+    if(!array)
+    {
+        return 0;
+    }
+
     run(program,new);
+    free(array);
     free(new);
     free(program);
     return (0);
